ce3b7e18e0ce307d08d4293a001d206400e83583
eduard <eduard42rus@mail.ru>
2023-05-11 23:11:07 +0700

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/lesson5/exceptionsmethod/ExceptionsmethodApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson5.exceptionsmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsmethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsmethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/lesson5/exceptionsmethod/ExceptionsmethodApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson5.exceptionsmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsmethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
